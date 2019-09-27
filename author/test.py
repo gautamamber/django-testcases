d1d14d7841906df6f9502a85394d40cb070c4b0c
gautamamber <ambergautam1@gmail.com>
2019-09-27 21:52:09 +0530

    feat():add more test cases

--- author/test.py
+++ author/test.py
@@ -11,10 +11,17 @@ class UserTestCase(TestCase):
         self.publisher = User.objects.create(
             username = "Mark",
             email = "amber+1@nickelfox.com",
-            user_type = User.PUBLISHER
+            user_type = User.PUBLISHER,
+            no_of_books = 12
         )
     def test_get_publisher(self):
         self.assertEquals(User.PUBLISHER, 2)
     
     def test_get_author(self):
         self.assertEqual(User.AUTHOR, 1)
+    
+    def test_can_write(self):
+        self.assertTrue(self.author.can_write_books())
+    
+    def test_can_write_2(self):
+        self.assertFalse(self.publisher.can_write_books())
