499ff5e3ac0b752041460ae6485eed439fb1a5aa
gautamamber <ambergautam1@gmail.com>
2019-09-28 15:16:41 +0530

    fix():change reverse import

--- author/test.py
+++ author/test.py
@@ -1,7 +1,7 @@
 from django.test import  TestCase, Client
 from .models import User
 from .forms import RegistrationForm
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 class UserTestCase(TestCase):
     def setUp(self):
